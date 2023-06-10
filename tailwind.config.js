0dd8cbc705fe6019ea4a1304bda11ee3b39c61c9
CodeBlower <saifulrmail@gmail.com>
2023-06-10 09:41:03 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'f': ['Pacifico', 'cursive'],
+      }
+    },
+  },
+  plugins: [],
+}
+
